095f3d088cd6f5f136c2e4acdffabd607c7a97a6
rustamsafarovrs <rs.programmer@hotmail.com>
2021-04-06 22:52:34 +0500

    project name changed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular10JwtAuth'`, () => {
+  it(`should have as title 'AngularJwtAuth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular10JwtAuth');
+    expect(app.title).toEqual('AngularJwtAuth');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular10JwtAuth app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AngularJwtAuth app is running!');
   });
 });
